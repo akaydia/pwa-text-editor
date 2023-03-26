8e289d4fd51420ab8c9ffa6266d17e65068dc1b4
Akaydia <akaydiaaa@hotmail.com>
2023-03-26 15:54:29 -0400

    added to database.js, putdb and getdb functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("post to the database");
+  const putDb =  await openDB('jate', 1);
+  const tx = putDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put(content);
+  request.onsuccess = () => {
+    console.log('success');
+  }
+  request.onerror = () => {
+    console.log('error');
+  }
+  await tx.done;
+  putDb.close();
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("get from the database");
+  const getDb = await openDB('jate', 1);
+  const tx = getDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  request.onsuccess = () => {
+    console.log('success');
+  }
+  request.onerror = () => {
+    console.log('error');
+  }
+  await tx.done;
+  getDb.close();
+};
 
 initdb();
